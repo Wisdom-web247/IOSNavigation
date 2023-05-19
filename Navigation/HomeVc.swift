de207f4d8b1d5666490f383546d4cf2dad5912e6
Wisdom Mahlangu <wizzymahlangu@gmail.com>
2023-05-19 16:09:35 +0200

    first commit

---
+++ Navigation/HomeVc.swift
@@ -0,0 +1,23 @@
+//
+//  HomeVc.swift
+//  Navigation
+//
+//  Created by DA MAC M1 138 on 2023/05/19.
+//
+
+import UIKit
+
+class HomeVc: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func btnPressed(_ sender: UIButton) {
+        
+        //performSegue(withIdentifier:"SecondViewController", sender: self)
+        
+    }
+}
